d6ada557397aad83ecce72e078d77f83fdba4b53
hairlyOwl <lxy_happy@126.com>
2023-06-09 16:35:57 +0800

    关键字、标识符、基础结构

---
+++ project01/main.go
@@ -0,0 +1,16 @@
+/*
+@Time : 2023/6/9 14:50
+@Author : 小哈
+@File : main
+@note: 基础结构
+*/
+package main
+
+import "fmt"
+
+// main()函数
+func main() {
+	fmt.Println("main start")
+	learnFunc()
+	fmt.Println("main end")
+}
---
+++ project01/structL.go
@@ -0,0 +1,36 @@
+/*
+@Time : 2023/6/9 14:50
+@Author : 小哈
+@File : structL
+@note: 基础结构
+*/
+//程序所属包
+package main
+
+//导入依赖包
+import "fmt"
+
+// 常量定义
+const NAME string = "golang"
+
+// 全局变量的声明与赋值
+var a string = "go语言"
+
+// 一般类型声明 相当于类型别名
+type aInt int
+
+var test aInt = 1
+
+// 结构的声明
+type Learn struct {
+}
+
+// 接口声明
+type Ilearn interface {
+}
+
+// 函数定义
+func learnFunc() {
+
+	fmt.Println("learnStruct", test)
+}
